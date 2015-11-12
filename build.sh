f755419c56c46d7ebbf9c089deda7b28206fc468
zhiguangq <qiuzhiguang@kugou.net>
2015-11-12 19:56:55 +0800

    build

--- build.sh
+++ build.sh
@@ -1,24 +1,33 @@
 #/bin/bash
 
-echo aa
+echo $PWD
 
 # build sdk
 cd sdk
-#rm -rf build
-#mkdir build
+rm -rf build
+mkdir build
 cd build
 cmake ..
 make
 
+# copy lib include
 cd ../..
 cp -rf ./src/RTPCaptureSDK.h ./demo/include/
+cp -rf ./../../lib/poco/libPocoFoundationd.a demo/lib/
+cp -rf ./../../lib/poco/libPocoNetd.a demo/lib/
+cp -rf ./../../lib/poco/libPocoUtild.a demo/lib/
+cp -rf ./../../lib/srs/libsrs_librtmp.a demo/lib/
 
-cd demo/cmake
-#rm -rf build
-#mkdir build
+# build demo
+cd demo
+rm -rf bin && mkdir bin
+rm -rf log && mkdir log
+rm -rf movie && mkdir movie
+
+cd cmake
 cmake .
 make
 
-cd ../../..
-cd demo
+# run demo
+cd ..
 ./bin/demo
